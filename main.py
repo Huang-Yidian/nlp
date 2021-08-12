940a98307f1c01f7dc600f462a925d9f152d4b79
Huang-Yidian <1208694059@qq.com>
2021-08-12 21:51:50 +0800

    2021.8.12 add data iter(return batch*[input,target,...]

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ if __name__ == "__main__":
         concate_file(config)
     if not os.path.exists(config.diction) and not os.path.exists(config.indexing):
         process_embedding(config)
+
+    dataloader = DataLoader('msra','bmeso')
+    train_data = dataloader.load_msra(config.batch_size,config.train_path)
+    print(dataloader.label)
+    for i,(batch,is_next_epoch) in enumerate(train_data):
+        print(batch[0][1])
+        exit()
--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -2,17 +2,17 @@ import numpy as np
 
 class DataLoader(object):
     
-    def __init__(self,type):
+    def __init__(self,type,label_type):
         super(DataLoader, self).__init__()
         self.type = type
-
+        self.label_type = label_type
+        self.label = []
         if type in ["msra","MSRA"]:
             self.msra = True
         else:
             raise Exception("DataLoader Error: Init with unknown type")
 
-
-    def load_msra(self,path):
+    def load_msra(self,batch_size,path):
         try:
             data = {}
             data['chars'] = []
@@ -25,6 +25,8 @@ class DataLoader(object):
                 if len(line) > 2:
                     char,target = line.strip().split()
                     chars.append(char)
+                    if target not in self.label:
+                        self.label.append(target)
                     targets.append(target)
                 else:
                     data['chars'].append(chars)
@@ -32,8 +34,44 @@ class DataLoader(object):
                     chars = []
                     targets = []
 
-            return data
+            dataset = Dataset(data,batch_size)
+            self.label.sort()
+            return dataset
         except FileNotFoundError:
             print("FileNotFoundError : File not found in",path)
 
 
+class Dataset(object):
+    def __init__(self,data : dict,batch, keys=['chars','targets']):
+        super(Dataset, self).__init__()
+        self.dataset = data
+        self.data_len = len(data.keys())
+        self.keys = keys
+        self.index = 0
+        self.batch = batch
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < self.data_len:
+            res = [[]]
+            for i in range(self.batch):
+                for key in self.keys:
+                    if key not in self.dataset.keys():
+                        raise Exception("Key %s of Dataset is wrong！" % (key))
+                    res[i].append(self.dataset[key][self.index])
+                res.append([])
+
+            self.index += 1
+            return res,False
+        else:
+            res = [[]]
+            for i in range(self.batch):
+                for key in self.keys:
+                    if key not in self.dataset.keys():
+                        raise Exception("Key %s of Dataset is wrong！" % (key))
+                    res[i].append(self.dataset[key][self.index])
+                res.append([])
+            return res, True
